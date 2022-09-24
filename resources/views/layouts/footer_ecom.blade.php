16b17bd42c6058e7321bff1cb4e3c14188bcd2b2
Jovan Lopez <everest.studio32@gmail.com>
2022-09-24 14:05:50 -0500

    main

--- resources/views/layouts/footer_ecom.blade.php
+++ resources/views/layouts/footer_ecom.blade.php
@@ -1,24 +1,39 @@
 
 <footer class=" flex flex-col w-full h-full px-10 bg-black text-white">
 
-    <div class="flex flex-wrap items-center justify-center gap-40">
+    <div class="flex flex-wrap items-center justify-center lg:gap-40">
 
-        <div class=" flex flex-col gap-5 py-10">
-            <h3 class=" text-lg font-semibold">Ingresa tu correo y recibe ofertas e informacion de nuestros productos.</h3>
+        <div class=" flex flex-col gap-5 py-10 w-96">
+            <h3 class=" text-lg font-semibold ">Ingresa tu correo y recibe ofertas e informacion de nuestros productos.</h3>
             <div class=" flex items-center">
                 <input type="email" class=" bg-transparent">
                 <button></button>
             </div>
         </div>
-        <div class="flex items-center flex-wrap py-10">
-            <h3>Ingresa tu correo y recibe ofertas o mas informacion de nuestros productos.</h3>
+        <div class="flex items-start flex-wrap py-10 gap-10 ">
+           <div class="w-60">
+            <h3 class=" text-lg font-semibold">Sucursales</h3>
+            <ul>
+                <li class="py-3">Av Nereo Rodríguez Barragán 850, Col del Valle 1ra Secc, 78200 San Luis, S.L.P.</li>
+                <li class="py-3">Calle Emiliano Zapata 265, Centro Historico, 78000 San Luis, S.L.P.</li>
+            </ul>
+           </div>
+           <div>
+            <h3 class=" text-lg font-semibold">Contacto</h3>
+            <ul>
+                <li class="py-3">contacto@bantaytactical.com</li>
+                <li class="py-3">+52 4445474356</li>
+            </ul>
+           </div>
         </div>
 
     </div>
 
     {{-- Todos los derechos reservados --}}
     <div class=" flex justify-center items-center">
-        <p class="py-3">Bantay Tactical </p>
+        
+        <p class="py-3 mr-2">Bantay Tactical</p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" style="fill: white;transform: ;msFilter:;"><path d="M13 9h-3v2h3a1 1 0 0 0 0-2z"></path><path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm2.13 15-2.67-4H10v4H8V7h5a3 3 0 0 1 .79 5.88L16.54 17z"></path></svg>
     </div>
 
 </footer>
